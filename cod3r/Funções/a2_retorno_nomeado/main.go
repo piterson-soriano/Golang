1a5f61784e13135fe4cce4063261ac241e650466
Pit-Soriano <piterson.1@hotmail.com>
2025-06-19 18:57:36 -0300

    Estudo

--- cod3r/Funções/a2_retorno_nomeado/main.go
+++ cod3r/Funções/a2_retorno_nomeado/main.go
@@ -1,13 +1,18 @@
 package main
 
-func trocar(p1, p2 int) (segundo int, primeiro int) {
-	segundo = p2
-	primeiro = p1
-	return // retorno limpo
+import "fmt"
 
+// Função que recebe dois inteiros e retorna dois inteiros com nomes
+func trocar(p1, p2 int) (segundo int, primeiro int) {
+	segundo = p2  // 'segundo' recebe o valor de p2
+	primeiro = p1 // 'primeiro' recebe o valor de p1
+	return        // Retorna os valores nomeados (segundo, primeiro)
 }
 
 func main() {
-	x, y := trocar(2, 3)
+	x, y := trocar(2, 3) // Chama a função com 2 e 3; x=3, y=2
+	fmt.Println(x, y)    // Imprime: 3 2
 
+	segundo, primeiro := trocar(7, 1) // Chama a função com 7 e 1; segundo=1, primeiro=7
+	fmt.Println(segundo, primeiro)    // Imprime: 1 7
 }
---
+++ cod3r/Funções/a3_primeira_classe/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+// Declara a variável soma e atribui a ela uma função anônima que recebe dois inteiros (a e b)
+
+var soma = func(a, b int) int {
+	return a + b //retorna a soma deles
+}
+
+func main() {
+	// Chama a função soma com os valores 2 e 3, e imprime o resultado: 5
+	fmt.Println(soma(2, 3))
+
+	// Declara uma nova função anônima chamada sub, que faz a subtração de dois inteiros
+	sub := func(a, b int) int {
+		return a - b
+	}
+	// Chama a função sub com os valores 2 e 3, e imprime o resultado: -1
+	fmt.Println(sub(2, 3))
+}
