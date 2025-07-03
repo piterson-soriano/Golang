3aa6a76bc738e615cb31b7f518d50f18162eee88
Piterson <piterson.1@hotmail.com>
2025-07-03 15:47:17 -0300

    Estudos

--- cod3r/Funções/a8_variatica_slice/main.go
+++ cod3r/Funções/a8_variatica_slice/main.go
@@ -5,11 +5,13 @@ import "fmt"
 func imprimirAprovados(aprovados ...string) {
 	fmt.Println("Lista de aprovados")
 	for i, aprovado := range aprovados {
+		// Imprime cada nome precedido de um número de ordem, começando por 1
 		fmt.Printf("%d) %s\n", i+1, aprovado)
 	}
 }
 
 func main() {
 	aprovados := []string{"Maria", "Pedro", "José", "Guilherme"}
+	// Chama a função imprimirAprovados, usando o operador "..." para expandir a slice em argumentos individuais
 	imprimirAprovados(aprovados...)
 }
