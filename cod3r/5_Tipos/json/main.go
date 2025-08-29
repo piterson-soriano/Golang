954c637f6118fc391e392132d3cd68e6b548f6e4
Piterson <piterson.1@hotmail.com>
2025-08-29 19:56:13 -0300

    Estudos

---
+++ cod3r/5_Tipos/json/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type produto struct {
+	//Identificador maiusculo, significa que é pública. Quando for minusculo, será privada.
+	ID    int      `json:"ID"`
+	Nome  string   `json:"nome"`
+	Preco float64  `json:"preco"`
+	Tags  []string `json:"tags"`
+}
+
+func main() {
+	//struct para json
+	p1 := produto{1, "Notebook", 1899.00, []string{"Promoção", "Eletrônico"}}
+	p1Json, _ := json.Marshal(p1)
+	fmt.Println(string(p1Json))
+}
